c66371a466674a03dc0250bf2635b5433de681c0
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-11-21 15:26:54 +0700

    Menambahkan state untuk alamat pada file Formulir.kt

--- app/src/main/java/com/example/arsitekturmvvm/view/Formulir.kt
+++ app/src/main/java/com/example/arsitekturmvvm/view/Formulir.kt
@@ -24,6 +24,7 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -44,7 +45,7 @@ fun FormIsian(
     modifier: Modifier = Modifier
 ){
     var txtNama by rememberSaveable { mutableStateOf("") }
-    
+    var txtAlamat by remember { mutableStateOf("") }
 
     Scaffold (modifier = Modifier,
         {
