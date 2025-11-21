d7f918ce9782561ac68d443cf9e12081ac31194c
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-11-21 15:27:24 +0700

    Menambahkan variabel state txtGender pada file Formulir.kt

--- app/src/main/java/com/example/arsitekturmvvm/view/Formulir.kt
+++ app/src/main/java/com/example/arsitekturmvvm/view/Formulir.kt
@@ -46,6 +46,8 @@ fun FormIsian(
 ){
     var txtNama by rememberSaveable { mutableStateOf("") }
     var txtAlamat by remember { mutableStateOf("") }
+    var txtGender by remember { mutableStateOf("") }
+    
 
     Scaffold (modifier = Modifier,
         {
