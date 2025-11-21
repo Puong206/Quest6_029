3f35ccdb3b11737c05c8a0c12b53b612018e00d8
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-11-21 15:51:05 +0700

    Menambahkan inisialisasi variabel uiState dengan collectAsState pada Navigasi.kt

--- app/src/main/java/com/example/arsitekturmvvm/view/uicontroller/Navigasi.kt
+++ app/src/main/java/com/example/arsitekturmvvm/view/uicontroller/Navigasi.kt
@@ -3,6 +3,7 @@ package com.example.arsitekturmvvm.view.uicontroller
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
@@ -25,6 +26,7 @@ fun DataApp(
     navController: NavHostController = rememberNavController()
 ){
     Scaffold { isiRuang ->
+        val uiState = viewModel.statusUI.collectAsState()
         NavHost(
             navController = navController,
             startDestination = Navigasi.Formulirku.name,
