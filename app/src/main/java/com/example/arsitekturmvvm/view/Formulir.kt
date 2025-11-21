08706858cfe72bd153d9c2f1c010179e6bb29c4e
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-11-21 15:26:16 +0700

    Menambahkan state variable txtNama dengan rememberSaveable pada Formulir.kt

--- app/src/main/java/com/example/arsitekturmvvm/view/Formulir.kt
+++ app/src/main/java/com/example/arsitekturmvvm/view/Formulir.kt
@@ -22,6 +22,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -39,7 +43,8 @@ fun FormIsian(
     onSubmitBtnClick: (MutableList<String>) ->  Unit,
     modifier: Modifier = Modifier
 ){
-
+    var txtNama by rememberSaveable { mutableStateOf("") }
+    
 
     Scaffold (modifier = Modifier,
         {
